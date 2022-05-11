b1949fd76ac5490db8eabed198e5862c564a616b
Madison Law <madisonlaw1@gmail.com>
2022-05-11 12:43:42 -0600

    Add onChange handler and state

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css'
 
 const Form = () => {
+
+  const [formFields, setFormFields] = useState({
+    title: '',
+    text: ''
+  })
+
+
+  const handleChange = (e) => {
+    setFormFields({
+      ...formFields,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
-    <h1>I'm a form!</h1>
+    <form className='form-container'>
+      <input 
+        type='text'
+        placeholder='enter text title'
+        name='title'
+        value={formFields.title}
+        onChange={(e) => handleChange(e)}
+        required
+      />
+      <input 
+        type='text'
+        placeholder='enter tl;dr text here'
+        name='text'
+        value={formFields.text}
+        onChange={(e) => handleChange(e)}
+        required
+      />
+      <button>submit!</button>
+
+    </form>
   );
 }
  
