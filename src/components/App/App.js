e6a38bd13ffe850373b26bfa4f80ee252faaef54
Madison Law <madisonlaw1@gmail.com>
2022-05-11 19:19:11 -0600

    Create submit method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import CardContainer from '../CardContainer/CardContainer'
 import './App.css';
 
 const App = () => {
+
+  const [tldrList, setTldrList] = useState([])
+
+  const submitTldr = (newTldr) => {
+    setTldrList([...tldrList, newTldr])
+  }
+
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar submitTldr={submitTldr}/>
       <CardContainer />
       
     </div>
