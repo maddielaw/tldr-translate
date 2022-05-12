69bcaeca9d781fd332807a097a97ab2898d1f1bb
Madison Law <madisonlaw1@gmail.com>
2022-05-11 20:02:04 -0600

    Create map to render cards dynamically

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './CardContainer.css'
 import Card from '../Card/Card'
 
-const CardContainer = () => {
+const CardContainer = ({ tldrList }) => {
+
+  const createTldrCards = () => {
+    return tldrList.map(tldr => {
+      return <Card id={tldr.id} key={tldr.id} title={tldr.title} tldr={tldr.tldr}/>
+    })
+  }
+
   return (
     <section className='card-container'>
       <h1>Your tl;drs</h1>
-      <Card />
-
+      {tldrList && createTldrCards()}
     </section>
   );
 }
