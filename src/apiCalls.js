bcf6fc1dab9589c03b427d316cff62497d03c150
Madison Law <madisonlaw1@gmail.com>
2022-05-12 19:15:37 -0600

    Reformat API call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,9 +2,29 @@ const apiKey = process.env.REACT_APP_OPENAI_KEY
 // console.log(apiKey)
 
 
-const fetchTldr = () => {
-  return fetch('http://localhost:3001/sightings')
-    .then(response => response.json())
+const packageAIData = (prompt) => {
+  const aiData = {
+    "prompt": prompt,
+    "temperature": 0.7,
+    "max_tokens": 60,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0
+  }
+  return aiData;
 }
 
-export default fetchTldr;
+
+const postTldr = (data) => {
+  return fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(data)
+  })
+}
+
+
+export { packageAIData, postTldr }
