30ae6dcf6ac7a23b0df82b24dbf4b12085b7ae73
Madison Law <madisonlaw1@gmail.com>
2022-05-12 18:33:06 -0600

    Add contional render

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Card.css'
 
 const Card = ({ title, tldr }) => {
-  return (
-    <section className='card'>
-      <h1>{title}</h1>
-      <p>{tldr}</p>
-    </section>
-  );
+  if (title && tldr) {
+    return (
+      <section className='card'>
+        <h1>{title}</h1>
+        <p>{tldr}</p>
+      </section>
+    );
+  }
 }
  
 export default Card;
