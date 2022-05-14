1e6e8bbfe4a54b3e15424832a93653951b3623e4
Madison Law <madisonlaw1@gmail.com>
2022-05-14 14:55:01 -0600

    Add delete btn

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Card.css'
 
-const Card = ({ title, originalText, tldr }) => {
+const Card = ({ id, title, originalText, tldr, deleteTldr }) => {
   if (title && tldr) {
     return (
       <section className='card'>
-        <h1>{title}</h1>
+        <div className='card-title-container'>
+          <h1>{title}</h1>
+          <button className='delete-btn' onClick={() => deleteTldr(id)}>X</button>
+        </div>
         <section className='card-text-container'>
           <div className='original-prompt'>
             <p className='original-prompt-header'>Original Text :</p>
